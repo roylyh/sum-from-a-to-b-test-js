155cd89c96792b23aabb713039e44471f886dd3d
roylyh <roy.louyihui@gmail.com>
2023-02-10 23:40:44 -0800

    Sum From a to b Recursively

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 
 function sum(fromN, toN) {
   // Sum all the values from fromN up to toN
+  let i = fromN;
+  while (i < toN) {
+    return i + sum(fromN + 1,toN);
+  }
+  return toN;
+
 }
 
 module.exports = sum;
